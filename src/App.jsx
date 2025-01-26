91cb77dbffdbc79b4c895a44a2a38a647d76dfd1
Artem Matvieientsev <tmatveentsev@gmail.com>
2025-01-26 13:32:17 +0100

    base form

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,9 @@
-import { useState } from "react";
-import LangSwitcher from "./LangSwitcher/LangSwitcher";
-import LoginForm from "./LoginForm/LoginForm";
-import FeedbackForm from "./FeedbackForm/FeedbackForm";
+import ControlledForm from "./components/Forms/ControlledForm";
 
 export default function App() {
-  // Колбек-функція для обробки сабміту форми
-  const handleLogin = (userData) => {
-    // Виконуємо необхідні операції з даними
-    console.log(userData);
-  };
-  const [lang, setLang] = useState("uk");
-
   return (
     <>
-      <h1>Please login to your account!</h1>
-      {/* Передаємо колбек як пропс форми */}
-      <LoginForm onLogin={handleLogin} />
-      <p>Selected language: {lang}</p>
-      <LangSwitcher value={lang} onSelect={setLang} />
-      <FeedbackForm />
+      <ControlledForm />
     </>
   );
 }
